d7f0f7ccb64134eaebc921655fc6bc73a631ae16
Fcode <fcode@protonmail.com>
2018-04-28 17:30:54 -0600

    Corregimos errores y efecutamos cambios

--- fuente/main.go
+++ fuente/main.go
@@ -10,21 +10,19 @@ func main() {
 		s           []byte
 		iteraciones int
 	)
-	fmt.Println("ingrese que desea realizar\n, suma : su, \n, resta: res,\n, multiplicacion" +
-		+" divicion: div \n")
+	fmt.Println("ingrese que desea realizar\n, suma : su, \n, resta: res,\n, multiplicacion: multi,\n,  divicion: div \n")
 	fmt.Scanln(&r)
 
 	for iteraciones == 0 {
 		switch r {
 
-		case salir:
+		case "salir":
 			{
 				fmt.Println("gracias por utilizar el programa... :=) ")
 				iteraciones++
 			}
 		default:
-			fmt.Println("los datos ingresados no son validos por favor buelva" +
-				+"a intentarlo")
+			fmt.Println("los datos ingresados no son validos por favor buelva" + "a intentarlo")
 		}
 	}
 
