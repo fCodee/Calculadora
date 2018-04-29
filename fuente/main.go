2f8daa1b1e3ab86bdaff2f4c50d01e24af72b1d2
Fcode <fcode@protonmail.com>
2018-04-28 18:30:07 -0600

    añadimos la funcionalidad de suma

--- fuente/main.go
+++ fuente/main.go
@@ -2,20 +2,54 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
 	var (
 		r           string
-		s           []byte
+		sumandos    []int
 		iteraciones int
+		contador    int
 	)
 	fmt.Println("ingrese que desea realizar\n, suma : su, \n, resta: res,\n, multiplicacion: multi,\n,  divicion: div \n")
 	fmt.Scanln(&r)
 
 	for iteraciones == 0 {
 		switch r {
+		case "su", "suma":
+			{
 
+				for iteraciones == 0 {
+					//imprimimos una sola vez el titulo al comienzo y almacenamos un dato
+					if iteraciones == 0 {
+						fmt.Println("ingrese numero")
+					}
+					fmt.Scanln(&r)
+					if r != "sum" && r != "suma" {
+						i, err := strconv.Atoi(r)
+						if err == nil {
+							fmt.Println("no podemos almacenar el dato ")
+							iteraciones++
+						} else {
+							sumandos[contador] = i
+							contador++
+						}
+
+					} else {
+						contador = 0
+						for i := 0; i < len(sumandos); i++ {
+							contador += sumandos[i]
+						}
+
+						fmt.Println("la suma de los numeros es \n\t : ", contador)
+						fmt.Scanln()
+					}
+
+				}
+
+				iteraciones = 0
+			}
 		case "salir":
 			{
 				fmt.Println("gracias por utilizar el programa... :=) ")
