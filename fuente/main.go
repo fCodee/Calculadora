93951299652506d5b18b2caa027c7c1289bc5bf6
Fcode <fcode@protonmail.com>
2018-04-28 19:29:08 -0600

    funcionalidad de suma correcta se ingresa la palabra <suma> o <su> o <sum> para obtener el resultado de los numeros ingresados

--- fuente/main.go
+++ fuente/main.go
@@ -9,25 +9,23 @@ func main() {
 	var (
 		r           string
 		sumandos    []int
-		iteraciones int
+		iteraciones int = 0
 		suma        int
 	)
-	fmt.Println("ingrese que desea realizar\n, suma : su, \n, resta: res,\n, multiplicacion: multi,\n,  divicion: div \n")
-	fmt.Scanln(&r)
 
 	for iteraciones == 0 {
+		fmt.Println("ingrese que desea realizar\n, suma : su, \n, resta: res,\n, multiplicacion: multi,\n,  divicion: div \n")
+		fmt.Scanln(&r)
 		switch r {
 
 		case "su", "suma":
 			{
 
-				for iteraciones == 0 {
-					//imprimimos una sola vez el titulo al comienzo y almacenamos un dato
-					if iteraciones == 0 {
-						fmt.Println("ingrese numero")
-					}
+				for iteraciones == 0 { //for-while interno case
+					fmt.Println("ingrese numero")
+
 					fmt.Scanln(&r)
-					if r != "sum" && r != "suma" {
+					if r != "sum" && r != "suma" && r != "su" {
 						i, err := strconv.Atoi(r)
 						if err != nil {
 							fmt.Println("no podemos almacenar el dato ")
@@ -36,24 +34,23 @@ func main() {
 						sumandos = append(sumandos, i)
 
 					} else {
+
 						for i := 0; i < len(sumandos); i++ {
 							suma += sumandos[i]
 						}
 
 						fmt.Println("la suma de los numeros es \n\t : ", suma)
-						iteraciones
-						fmt.Scanln()
+						iteraciones++ //for-while interno case finaliza
 					}
-
 				}
 
+				//iteraciones = 1 //finaliza for-while externo
 				iteraciones = 0
 			}
 		case "salir":
-			{
-				fmt.Println("gracias por utilizar el programa... :=) ")
-				iteraciones++
-			}
+
+			fmt.Println("gracias por utilizar el programa... :=) ")
+			iteraciones++
 		default:
 			fmt.Println("los datos ingresados no son validos por favor buelva a intentarlo")
 		}
